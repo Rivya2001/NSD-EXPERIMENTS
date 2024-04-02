008babd83581d3893639c48e47d93622cfbe7821
Rivya2001 <113901262+Rivya2001@users.noreply.github.com>
2024-04-02 15:54:11 +0530

    Add files via upload

---
+++ name.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "enter the name:"
+read name
+echo "enter roll number:"
+read roll
+echo "enter the mark:"
+read mark
+echo ".........."
+echo "DETAILS"
+echo "name is : $name"
+echo "Roll number is:$roll"
+echo "mark is:$mark"
